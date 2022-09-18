8a74498780b00742c8bbbd4cec925df4a242f251
anastasia.razumova <ra@affise.com>
2022-09-18 15:50:19 +0200

    add test

---
+++ src/test/java/com/demoqa/StudentRegForm.java
@@ -0,0 +1,63 @@
+package com.demoqa;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class StudentRegForm {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void simpleTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("ivani@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1980");
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__day--015").click();
+        $("#subjectsInput").setValue("English").pressTab();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#currentAddress").setValue("Address123");
+        $(byText("Select State")).click();
+        $(byText("NCR")).click();
+        $(byText("Select City")).click();
+        $(byText("Delhi")).click();
+        $("#submit").click();
+
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Ivan Ivanov"));
+        $(".table-responsive").shouldHave(text("ivani@gmail.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("1234567890"));
+        $(".table-responsive").shouldHave(text("15 June,1980"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("Address123"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
+
+
+
+
+
+    }
+}
+
