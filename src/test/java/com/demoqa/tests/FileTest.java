88bd08782cda695bbe9af7a92e741d349ab0bff9
anastasia.razumova <ra@affise.com>
2022-10-13 13:53:15 +0200

    add FileTest

--- src/test/java/com/demoqa/tests/FileTest.java
+++ src/test/java/com/demoqa/tests/FileTest.java
@@ -8,7 +8,7 @@ import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FileTests {
+public class FileTest {
 
     ClassLoader cl = FileTest.class.getClassLoader();
 
@@ -17,19 +17,19 @@ public class FileTests {
         InputStream is = cl.getResourceAsStream("pdf1.zip");
         ZipInputStream zis = new ZipInputStream(is);
         ZipEntry entry;
-        while((entry = zis.getNextEntry()) != null) {
+        while ((entry = zis.getNextEntry()) != null) {
             String name = entry.getName();
             assertThat(entry.getName()).contains("test");
 
-            }
         }
+    }
 
     @Test
     void csvTest() throws Exception {
         InputStream is = cl.getResourceAsStream("csv.zip");
         ZipInputStream zis = new ZipInputStream(is);
         ZipEntry entry;
-        while((entry = zis.getNextEntry()) != null) {
+        while ((entry = zis.getNextEntry()) != null) {
             String name = entry.getName();
             assertThat(entry.getName()).contains("addresses");
 
@@ -41,13 +41,11 @@ public class FileTests {
         InputStream is = cl.getResourceAsStream("xls.zip");
         ZipInputStream zis = new ZipInputStream(is);
         ZipEntry entry;
-        while((entry = zis.getNextEntry()) != null) {
+        while ((entry = zis.getNextEntry()) != null) {
             String name = entry.getName();
             assertThat(entry.getName()).contains("tests-example");
 
         }
     }
 
-
-    }
-
+}
